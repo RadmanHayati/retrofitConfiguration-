69044e1a6c3ec9d7f8a94cd7d00414a790ca6975
Radman Hayati <radman.hayatii@gmail.com>
2021-02-14 22:46:33 +0330

    hello humans

---
+++ app/src/main/java/com/example/retrofitconfiguration/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.retrofitconfiguration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+/*
+GIT
+1-create a git repository locally : VCS > enable version control
+integration
+2-commit stuff: project directory > git > Add > commit directory
+2.1-terminal > git status > git commit -am "i did it"
+3-create it online dude > by github's user interface
+4-copy https
+5-VCS>Push>master default remote
+6 -insert https
+7- you got it
+ */
