bded16cde41092279691b33d0db89e92e7e1c506
Radman Hayati <radman.hayatii@gmail.com>
2021-02-14 23:29:22 +0330

    dependencies

--- app/src/main/java/com/example/retrofitconfiguration/MainActivity.kt
+++ app/src/main/java/com/example/retrofitconfiguration/MainActivity.kt
@@ -15,7 +15,7 @@ GIT
 1-create a git repository locally : VCS > enable version control
 integration
 2-commit stuff: project directory > git > Add > commit directory
-2.1-terminal > git status > git commit -am "i did it"
+2.1- now you can use terminal > git status > git commit -am "i did it"
 3-create it online dude > by github's user interface
 4-copy https
 5-VCS>Push>master default remote
